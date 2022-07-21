558888a07492ca09239ce95d21c39ec5ed72f289
Weariful <guiguifeuser@gmail.com>
2022-07-21 17:32:14 -0300

    add missing model

--- src/redis/models/index.ts
+++ src/redis/models/index.ts
@@ -5,6 +5,7 @@ import Game from "./Game";
 import Guild from "./Guild";
 import GuildPermission from "./GuildPermission";
 import Player from "./Player";
+import PlayerAction from "./PlayerAction";
 import PlayerTrack from "./PlayerTrack";
 import Playlist from "./Playlist";
 import PlaylistSong from "./PlaylistSong";
@@ -13,13 +14,13 @@ import User from "./User";
 import UserConnection from "./UserConnection";
 import Vote from "./Vote";
 
-// @ts-ignore
 const Models: Record<Prisma.ModelName, IModel> = {
   Blacklist,
   Game,
   Guild,
   GuildPermission,
   Player,
+  PlayerAction,
   PlayerTrack,
   Playlist,
   PlaylistSong,
